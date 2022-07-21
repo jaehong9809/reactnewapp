4f29a7b7698f8088cf6076204d65b381d1b50f56
LeeJaeHong <0915jhlee@daum.net>
2022-07-21 18:26:34 +0900

    b

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Detail from "./routes/detail";
 import { BrowserRouter as Router, Switch, Route, Routes } from "react-router-dom";
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/movie/:id" element={<Detail />} />
         <Route path="/" element={<Home />} />
